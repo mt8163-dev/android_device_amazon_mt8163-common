0fef59ab35361a7041595c7713f67d1de39786c0
R0rt1z2 <rogerortizleal@gmail.com>
2019-09-10 23:04:52 +0200

    ROM Is booting now.

---
+++ device.mk
@@ -0,0 +1,105 @@
+DEVICE_BASE := device/amazon/douglas
+DEVICE_VENDOR := /vendor/amazon/douglas
+
+# Device overlay
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_BASE)/overlay
+
+# Install init.d scripts
+PRODUCT_COPY_FILES += \
+    $(DEVICE_BASE)/configs/99exfat-support:system/etc/init.d/99exfat-support
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_BASE)/rootdir,root)
+
+# Config files
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    $(DEVICE_BASE)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(DEVICE_BASE)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    #$(DEVICE_BASE)/configs/mtk_omx_core.cfg:system/etc/mtk_omx_core.cfg
+
+# Audio
+PRODUCT_PACKAGES += \
+	audio.a2dp.default \
+    audio.r_submix.default \
+    audio.usb.default \
+    audio_policy.stub \
+    libalsautils \
+    libaudio-resampler \
+    libtinyalsa \
+    libtinycompress \
+    libtinyxml \
+    libaudioroute
+    
+# Bluetooth
+PRODUCT_PACKAGES += \
+	bluetooth.default
+	
+# Graphics
+PRODUCT_PACKAGES += \
+    libion \
+    libgralloc_extra \
+    libcap
+
+# Lights
+#PRODUCT_PACKAGES += \
+    lights.mt8163
+
+# Power
+PRODUCT_PACKAGES += \
+	power.default
+
+# network
+PRODUCT_PACKAGES += \
+    netd
+
+# Lights
+PRODUCT_PACKAGES +=\
+    lights.mt8163
+    
+# IPv6 tethering
+PRODUCT_PACKAGES += \
+    ebtables \
+    ethertypes
+
+# Zygote
+PRODUCT_PROPERTY_OVERRIDES += \
+    	ro.zygote=zygote64_32
+
+WITH_EXFAT := true
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# call dalvik heap config
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+
+# call hwui memory config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
+$(call inherit-product-if-exists, vendor/amazon/douglas/douglas-vendor.mk)
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo cm_douglas-user
+add_lunch_combo cm_douglas-userdebug
+add_lunch_combo cm_douglas-eng
