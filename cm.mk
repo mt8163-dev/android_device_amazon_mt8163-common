09804fd69e63a2e45d1c5cb767761faa9f4afc56
R0rt1z2 <rogerortizleal@gmail.com>
2019-10-08 22:07:51 +0200

    Override build description

--- cm.mk
+++ cm.mk
@@ -10,3 +10,10 @@ PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_NAME := cm_douglas
 PRODUCT_RELEASE_NAME := douglas
+
+# Override the fingerprint for SafetyNet
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=google/douglas/douglas:5.1.1/LVY48F/50.6.3.6_user_636558520:user/release-keys \
+    BUILD_FINGERPRINT=Amazon/douglas/douglas:5.1.1/LVY48F/50.6.3.6_user_636558520:user/release-keys \
+    PRIVATE_BUILD_DESC="douglas-user 5.1.1 LVY48F 50.6.3.6_user_636558520 release-keys"
+    PRIVATE_BUILD_DESC="douglas-user 5.1.1 LVY48F 50.6.3.6_user_636558520 release-keys"
