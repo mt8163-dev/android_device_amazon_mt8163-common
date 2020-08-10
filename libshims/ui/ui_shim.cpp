4cccd61283b3d6246466943b1fc3851c88a06f2e
fire855 <thefire855@gmail.com>
2020-08-10 17:25:07 +0200

    douglas: Handle more missing GraphicBuffer symbols.
    * E HAL     : dlopen failed: cannot locate symbol "_ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handleiRKNS_4RectEP13android_ycbcr" referenced by "/system/lib/libmtk_mmutils.so"...

--- libshims/ui/ui_shim.cpp
+++ libshims/ui/ui_shim.cpp
@@ -8,3 +8,13 @@ extern "C" {
         _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(handle, static_cast<uint32_t>(usage), bounds, vaddr);
     }
 }
+
+extern "C" {
+    void _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(buffer_handle_t, uint32_t, const android::Rect&, android_ycbcr*);
+    void _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handleiRKNS_4RectEP13android_ycbcr(buffer_handle_t, int, const android::Rect&, android_ycbcr*);
+
+    void _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handleiRKNS_4RectEP13android_ycbcr(buffer_handle_t handle, int usage, const android::Rect& bounds, android_ycbcr *ycbcr) {
+        _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(handle, static_cast<uint32_t>(usage), bounds, ycbcr);
+    }
+}
+
