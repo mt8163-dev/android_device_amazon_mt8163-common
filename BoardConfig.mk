5106b31cdfa471fd2deda6905bce7606ae8b3faa
R0rt1z2 <rogerortizleal@gmail.com>
2020-08-13 11:02:41 +0200

    douglas: Shim missing UI symbols.
    * CANNOT LINK EXECUTABLE "/system/bin/guiext-server": cannot locate symbol "_ZN7android19GraphicBufferMapper4lockEPK13native_handleiRKNS_4RectEPPv" referenced by "/system/lib64/libui_ext.so"...
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,7 +145,9 @@ LINKER_FORCED_SHIM_LIBS := \
     /system/lib/libcam.utils.sensorlistener.so|libshim_camera.so \
     /system/lib64/libcam.utils.sensorlistener.so|libshim_camera.so \
     /system/lib/libdrmmtkutil.so|libshim_drm.so \
-    /system/lib64/libdrmmtkutil.so|libshim_drm.so
+    /system/lib64/libdrmmtkutil.so|libshim_drm.so \
+    /system/lib/libui_ext.so|libshim_ui.so \
+    /system/lib64/libui_ext.so|libshim_ui.so
 
 # Device-Specific Headers
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_FOLDER)/include
