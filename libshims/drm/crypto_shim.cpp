fc0580538216698c57751e05e1187ac9dddeb502
Joel Stein <js@funkturm.net>
2020-08-14 15:44:19 +0200

    douglas: Shim missing OpenSSL symbols.
    * CANNOT LINK EXECUTABLE "/system/bin/amzn_dha_hmac": cannot locate symbol "OBJ_cleanup" referenced by "/system/bin/amzn_dha_hmac"...
    * ......
    * ........

--- libshims/drm/crypto_shim.cpp
+++ libshims/drm/crypto_shim.cpp
@@ -1,3 +1,17 @@
 #include <openssl/rsa.h>
+#include <openssl/mem.h>
+#include <openssl/cipher.h>
 
 extern "C" void CONF_modules_unload(int p) {}
+extern "C" void OBJ_cleanup(void) {}
+
+void *CRYPTO_malloc(size_t size) {
+	return OPENSSL_malloc(size);
+}
+
+void *CRYPTO_realloc(void *ptr, size_t new_size) { 
+	return OPENSSL_realloc(ptr, new_size);
+}
+void CRYPTO_free(void *ptr) { 
+	return OPENSSL_free(ptr);
+}
