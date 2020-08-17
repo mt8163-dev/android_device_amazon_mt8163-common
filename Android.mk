211d4cf93d23acbcdafcc712017cc62873acf9d8
R0rt1z2 <rogerortizleal@gmail.com>
2020-08-17 16:11:43 +0200

    douglas: Delete useless comments.
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- Android.mk
+++ Android.mk
@@ -14,12 +14,6 @@
 # limitations under the License.
 #
 
-# WARNING: Everything listed here will be built on ALL platforms,
-# including x86, the emulator, and the SDK.  Modules must be uniquely
-# named (liblights.tuna), and must build everywhere, or limit themselves
-# to only building on ARM if they include assembly. Individual makefiles
-# are responsible for having their own logic, for fine-grained control.
-
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),douglas)
--- cm.mk
+++ cm.mk
@@ -1,5 +1,3 @@
-## Specify phone tech before including full_phone
-
 # Release name
 PRODUCT_RELEASE_NAME := KFDOWI
 
