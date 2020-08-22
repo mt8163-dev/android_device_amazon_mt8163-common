9a3eab8dd28875dd6cdacdad95b563b1f061bd7e
R0rt1z2 <rogerortizleal@gmail.com>
2020-08-22 16:31:13 +0200

    douglas: Copy prebuilt MediaTek Keylayout.
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- device_douglas.mk
+++ device_douglas.mk
@@ -56,6 +56,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
 
+# Prebuilt Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayouts/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl
+
 # Wifi
 PRODUCT_PACKAGES += \
     libwpa_client \
