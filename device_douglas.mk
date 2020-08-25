603b9a45376101aecd8c4ce66f38691034e75767
R0rt1z2 <rogerortizleal@gmail.com>
2020-08-25 21:58:12 +0200

    douglas: Kang Media Codec Configuration Files from karnak.
    Those libraries are kanged from karnak's system dump (karnak-user 6.0 NS6314 2315 amz-p,release-keys) (FireOS 6.3.1.4).
    
    - [0] Fixes sound throught stock player (Eleven).
    - [1] Fixes SW decoding (2/2).
    - [2] Improves sound responsibity/quality/stability.
    - [3] Fixes notification sounds.
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- device_douglas.mk
+++ device_douglas.mk
@@ -53,6 +53,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_mediatek_audio.xml:system/etc/media_codecs_mediatek_audio.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
