c6fcd3dba09d688d2fb59a0d1705496ca3be792d
Michael W <baddaemon87@gmail.com>
2020-10-25 09:51:06 +0100

    douglas: Inherit product_launched_with_l.mk

--- cm.mk
+++ cm.mk
@@ -5,6 +5,7 @@ PRODUCT_RELEASE_NAME := KFDOWI
 $(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/amazon/douglas/device_douglas.mk)
