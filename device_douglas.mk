c9c069905e6f277d5ac89a0b655aaa0b9d95941f
R0rt1z2 <rogerortizleal@gmail.com>
2020-11-08 11:14:34 +0100

    douglas: Copy resize2fs_static.
    * Running /sbin/resize2fs_static on /dev/block/platform/mtk-msdc.0/by-name/userdata.
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- device_douglas.mk
+++ device_douglas.mk
@@ -128,7 +128,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.douglas.rc:root/init.douglas.rc \
     $(LOCAL_PATH)/rootdir/init.wifi.rc:root/init.wifi.rc \
     $(LOCAL_PATH)/rootdir/ueventd.mt8163.rc:root/ueventd.mt8163.rc \
-    $(LOCAL_PATH)/rootdir/sbin/resize_ext4:root/sbin/resize_ext4
+    $(LOCAL_PATH)/rootdir/sbin/resize_ext4:root/sbin/resize_ext4 \
+    $(LOCAL_PATH)/rootdir/sbin/resize2fs_static:root/sbin/resize2fs_static
 
 # Permissions
 PRODUCT_COPY_FILES += \
