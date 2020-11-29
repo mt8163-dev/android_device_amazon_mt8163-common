2a1835e0acfc74d2711b60b2366e00483ac80fc2
Joel Stein <js@funkturm.net>
2020-11-29 14:48:42 +0100

    douglas: Change PRODUCT_BRAND to google.
    * To pass play-certification.

--- cm.mk
+++ cm.mk
@@ -13,6 +13,6 @@ $(call inherit-product, device/amazon/douglas/device_douglas.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := douglas
 PRODUCT_NAME := cm_douglas
-PRODUCT_BRAND := Amazon
+PRODUCT_BRAND := google
 PRODUCT_MODEL := Fire HD8 2017
 PRODUCT_MANUFACTURER := Amazon
