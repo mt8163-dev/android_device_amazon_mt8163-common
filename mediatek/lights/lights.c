5b0be92c27ecd245473433cbed6fac98550dfb96
R0rt1z2 <rogerortizleal@gmail.com>
2021-08-24 17:08:58 +0200

    mt8163-common: lights: supress write_int spam
    * These log messages are just useless.
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- mediatek/lights/lights.c
+++ mediatek/lights/lights.c
@@ -175,7 +175,6 @@ write_int(char const* path, int value)
 #endif
 
     fd = open(path, O_RDWR);
-	ALOGD("write_int open fd=%d\n", fd);
     if (fd >= 0) {
         char buffer[20];
         int bytes = sprintf(buffer, "%d\n", value);
