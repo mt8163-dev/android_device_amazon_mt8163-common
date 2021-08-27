588a557097281ce79e74298023751cdda354ac9c
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2021-08-27 13:59:29 +0200

    mt8163-common: build Display/Graphics HIDL HAL

--- mt8163-common.mk
+++ mt8163-common.mk
@@ -109,7 +109,12 @@ PRODUCT_PACKAGES += \
 
 # Graphics
 PRODUCT_PACKAGES += \
-    libion
+    libion \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.renderscript@1.0-impl
 
 # Charger
 PRODUCT_PACKAGES += \
