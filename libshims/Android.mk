8d29f546102e497563bb80cd962de5d07d762250
Joel Bacchus <joel.bacchus@protonmail.com>
2021-08-28 00:21:05 +0200

    mt8163-common: libshims: update camera shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -30,7 +30,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := camera/camera_shim.cpp
-LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils
+LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils libsensor
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
