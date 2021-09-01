04b6366d4471ee1d32234ca60edbea3cddc1cd39
Bauuuuu <abansode@outlook.com>
2021-09-01 15:05:27 +0200

    mt8163-common: switch to Camera2

--- mt8163-common.mk
+++ mt8163-common.mk
@@ -30,7 +30,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
+    Camera2 \
     libcamera_parameters_mtk \
     libcam.halsensor \
     android.hardware.camera.provider@2.4-impl-legacy
