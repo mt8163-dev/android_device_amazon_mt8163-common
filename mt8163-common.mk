480b3b06658e69407aad605cc302fcfd5b65f963
joshi3 <joshi.naga3@gmail.com>
2021-09-03 12:01:14 +0200

    mt8163-common: build mediatek sensor hidl
    * Rewrote sensor hidl hal by kalyan.

--- mt8163-common.mk
+++ mt8163-common.mk
@@ -146,8 +146,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-impl-mediatek \
+    android.hardware.sensors@1.0-service-mediatek
 
 # Charger
 PRODUCT_PACKAGES += \
