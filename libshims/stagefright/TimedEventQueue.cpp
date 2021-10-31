4647888f9de8e735ad84d75435f7e92834c2446d
Roger Ortiz <rogerortizleal@gmail.com>
2021-10-31 21:11:35 +0100

    mt8163-common: libshims: extend TimedQueue symbols
    Signed-off-by: Roger Ortiz <rogerortizleal@gmail.com>

--- libshims/stagefright/TimedEventQueue.cpp
+++ libshims/stagefright/TimedEventQueue.cpp
@@ -11,11 +11,23 @@
 
 namespace android {
 
+extern "C" bool mRunning;
+
 extern "C" {
    /*
     * NOTE: TimedEventQueue was deprecated in Android 6.0 +.
     * This library provides stubs for TimedEventQueue symbols.
     */
+   bool mRunning;
+
+   void _ZN7android15TimedEventQueueC1Ev() {
+       ALOGV("(%s): TimedEventQueue init...", __func__);
+   }
+
+   void _ZN7android15TimedEventQueueD1Ev() {
+       ALOGI("(%s): TimedEventQueue exit...", __func__);
+   }
+
    int32_t _ZN7android15TimedEventQueue14postTimedEventERKNS_2spINS0_5EventEEEx(void *event, 
        int64_t realtime_us) {
          return realtime_us; // event->eventID();
@@ -23,6 +35,24 @@ extern "C" {
 
    int32_t _ZN7android15TimedEventQueue9postEventERKNS_2spINS0_5EventEEE(void *event) {
        return _ZN7android15TimedEventQueue14postTimedEventERKNS_2spINS0_5EventEEEx(event, 0);
+   }
+
+   int32_t _ZN7android15TimedEventQueue18postEventWithDelayERKNS_2spINS0_5EventEEEx(void *event,
+     int64_t delay_us) {
+       return _ZN7android15TimedEventQueue14postTimedEventERKNS_2spINS0_5EventEEEx(event, delay_us);
+   }
+
+   void _ZN7android15TimedEventQueue5startEv() {
+       mRunning = true;
+   }
+
+   void _ZN7android15TimedEventQueue4stopEb(bool flush) {
+       if (!flush) _ZN7android15TimedEventQueue14postTimedEventERKNS_2spINS0_5EventEEEx(NULL, INT64_MIN);
+       mRunning = false;
+   }
+
+   bool _ZN7android15TimedEventQueue11cancelEventEi(int32_t event_id) {
+       return (event_id == 0); 
    }
 }
 
