3e331838ffa4217de3a47fd2e6a35ebb25bfb81b
Amit Pundir <amit.pundir@linaro.org>
2022-06-19 13:26:52 +0200

    mt8163-common: Add gatekeeper software HIDL service
    Add gatekeeper software HIDL service.

--- mt8163.mk
+++ mt8163.mk
@@ -173,8 +173,7 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-service \
-    android.hardware.gatekeeper@1.0-impl
+    android.hardware.gatekeeper@1.0-service.software
 
 # Graphics
 PRODUCT_PACKAGES += \
