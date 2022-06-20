4c3d4e6ef38f62a6cd7a1bfb1f1ef094d7d07856
Alexander Koskovich <zvnexus@outlook.com>
2022-06-20 16:05:57 +0200

    mt8163-common: Add property to define OpenGL ES libraries
    * libEGL_${ro.hardware.egl}.so
      libGLESv1_CM_${ro.hardware.egl}.so
      libGLESv2_${ro.hardware.egl}.so

--- mt8163-common.mk
+++ mt8163-common.mk
@@ -117,6 +117,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
 
 # Wi-Fi
