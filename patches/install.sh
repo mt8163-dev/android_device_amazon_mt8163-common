2df57d6e1ae4a2fb7fbf315f5c8dfa09a4913296
Kai Jones <joneskai626@gmail.com>
2022-06-24 20:11:19 +0200

    mt8163-common: patches: disable getPresentationPosition

--- patches/install.sh
+++ patches/install.sh
@@ -2,7 +2,7 @@
 
 C=$(pwd)
 S="amazon/mt8163-common"
-D="frameworks/base hardware/libhardware"
+D="frameworks/base hardware/libhardware hardware/interfaces"
 
 apply_patches() { cd ${C}/${1}; git apply --ignore-whitespace ${C}/device/${S}/patches/$1/*.patch; cd ${C}; }
 
--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 C=$(pwd)
-D="frameworks/base hardware/libhardware"
+D="frameworks/base hardware/libhardware hardware/interfaces"
 
 clear_patches() { cd ${C}/${1}; git checkout -- . && git clean -df; cd ${C}; }
 
