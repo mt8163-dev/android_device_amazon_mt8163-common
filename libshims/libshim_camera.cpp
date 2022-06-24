ee937799efe9b83ae86f458764a0d71e8b617a84
zeelog <zeelog@users.noreply.github.com>
2022-06-24 12:56:08 +0200

    mt8163-common: libshims: shim more GraphicBuffer symbols

--- libshims/libshim_camera.cpp
+++ libshims/libshim_camera.cpp
@@ -1,5 +1,7 @@
 #include <string>
 
+#include <ui/GraphicBuffer.h>
+
 extern "C" {
     void _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
             void *(pthis), uint32_t inWidth, uint32_t inHeight, int inFormat,
@@ -10,3 +12,28 @@ extern "C" {
             pthis, inWidth, inHeight, inFormat, inUsage, "<Unknown>");
     }
 }
+
+extern "C" void _ZN7android13GraphicBufferC1EjjijjjP13native_handleb(
+        const native_handle_t* handle,
+        android::GraphicBuffer::HandleWrapMethod method,
+        uint32_t width,
+        uint32_t height,
+        int format,
+        uint32_t layerCount,
+        uint64_t usage,
+        uint32_t stride);
+
+extern "C" void _ZN7android13GraphicBufferC1EjjijjP13native_handleb(
+        uint32_t inWidth,
+        uint32_t inHeight,
+        int inFormat,
+        uint32_t inUsage,
+        uint32_t inStride,
+        native_handle_t* inHandle,
+        bool keepOwnership)
+{
+    android::GraphicBuffer::HandleWrapMethod inMethod =
+        (keepOwnership ? android::GraphicBuffer::TAKE_HANDLE : android::GraphicBuffer::WRAP_HANDLE);
+    _ZN7android13GraphicBufferC1EjjijjjP13native_handleb(inHandle, inMethod, inWidth, inHeight,
+        inFormat, static_cast<uint32_t>(1), static_cast<uint64_t>(inUsage), inStride);
+} 
