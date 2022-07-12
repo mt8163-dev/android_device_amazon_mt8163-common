bd5d92ce4ffcbe6334bc1875cf68775887f937ee
Nolen Johnson <johnsonnolen@gmail.com>
2022-07-12 11:23:00 +0200

    mt8163-common: Set Bootanimation to half-res
    * Faster boot, smoother animation.

--- mt8163.mk
+++ mt8163.mk
@@ -25,6 +25,9 @@ PRODUCT_SOONG_NAMESPACES += \
 # Enable DM file pre-opting to reduce first boot time
 PRODUCT_DEX_PREOPT_GENERATE_DM_FILES := true
 
+# Bootanimation
+TARGET_BOOTANIMATION_HALF_RES := true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
