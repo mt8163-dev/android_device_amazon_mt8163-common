a9e949d6cbb7c129eaf4ff4b10a019edf8d35850
R0rt1z2 <rogerortizleal@gmail.com>
2022-07-21 20:32:25 +0300

    mt8163-common: switch to giza audio stack
    * Both douglas and giza are almost identical in terms
      of hardware, which means we can use giza firmware in
      douglas and vice versa without major problems.
    
    * Turns out giza doesn't support Alexa, which means its
      audio stack is completely clean when it comes to audio
      algorithms and speech recognition stuff.
    
    * Let's switch to giza's audio stack so can get rid of
      all the alexa related shit without breaking audio at
      all.
    
    * This means we can also drop all the asp libraries/shims
      since they won't be used anymore by the new audio HAL.

--- libshims/libshim_binder.cpp
+++ libshims/libshim_binder.cpp
@@ -9,16 +9,3 @@ extern "C" void _ZNK7android10IInterface8asBinderEv(void *retval, void *self) {
     android::sp<android::IBinder> *ret = new(retval) android::sp<android::IBinder>;
     *ret = android::IInterface::asBinder((android::IInterface*)self);
 }
-
-extern "C" {
-    extern int _ZNK7android6Parcel18readFileDescriptorEv();
-    extern int _ZN7android6Parcel25writeParcelFileDescriptorEib();
-
-    int _ZNK7android6Parcel24readParcelFileDescriptorERi(int& outCommChannel) {
-        return _ZNK7android6Parcel18readFileDescriptorEv();
-    }
-
-    int _ZN7android6Parcel25writeParcelFileDescriptorEii(int& outCommChannel) {
-        return _ZN7android6Parcel25writeParcelFileDescriptorEib();
-    }
-}
