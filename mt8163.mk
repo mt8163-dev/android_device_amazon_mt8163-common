1df5c83d5e1fce6943cebf71a18b8889f50ad2aa
Bruno Martins <bgcngm@gmail.com>
2022-07-23 14:57:31 +0300

    mt8163-common: Build common basic USB HAL

--- mt8163.mk
+++ mt8163.mk
@@ -198,8 +198,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-impl \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
 
 # Keymaster
 PRODUCT_PACKAGES += \
