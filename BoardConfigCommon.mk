9409e733e4733ab5d5e0faf4bff3ce5a826864a4
Jeff Sharkey <jsharkey@android.com>
2022-07-28 13:37:47 +0200

    mt8163-common: Enable partitions on loop devices
    This is used by the new "virtual disk" feature used by vold to help
    debug and test storage code on devices without physical media.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,6 +63,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --board ""
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
