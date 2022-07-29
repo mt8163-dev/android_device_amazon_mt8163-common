04eb36f0d9892f1bdd2d69f8983c866267886959
R0rt1z2 <rogerortizleal@gmail.com>
2022-07-29 21:19:30 +0200

    mt8163-common: update GraphicBufferMapper::lockYCbCr signature

--- extract-files.sh
+++ extract-files.sh
@@ -80,6 +80,9 @@ function blob_fixup() {
         lib*/hw/hwcomposer.mt8163.so)
             sed -i 's|_ZN7android19GraphicBufferMapper4lockEPK13native_handleiRKNS_4RectEPPv|_ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv|g' "${2}"
             ;;
+        lib*/libmtk_mmutils.so)
+            sed -i 's|_ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handleiRKNS_4RectEP13android_ycbcr|_ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr|g' "${2}"
+            ;;
         vendor/lib/libstagefright_soft_ddpdec.so)
             patchelf --add-needed "libstagefright_softomx.so" "${2}"
             ;;
