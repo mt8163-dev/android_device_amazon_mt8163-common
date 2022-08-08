5f526c9bf66db1cb83e494938aa61ad0608fd642
Amit Pundir <amit.pundir@linaro.org>
2022-08-08 16:53:07 +0300

    mt8163-common: Copy mediaswcodec.policy

--- mt8163.mk
+++ mt8163.mk
@@ -127,7 +127,8 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/configs/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/configs/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
 
 # Apex
 PRODUCT_COPY_FILES += \
