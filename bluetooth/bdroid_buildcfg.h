5b71da16dd3dca3a3d3bc7f36812a6ce292a3bec
R0rt1z2 <me@r0rt1z2.com>
2023-05-18 12:45:53 +0200

    mt8163-common: bluetooth: Load `BTM_DEF_LOCAL_NAME` dynamically
    Co-authored-by: luca020400 <luca.stefani.ge1@gmail.com>
    Co-authored-by: Kyle Harrison <khwebmail@gmail.com>
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Co-authored-by: R0rt1z2 <me@r0rt1z2.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,7 +1,32 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Amazon Fire HD8"
+#include <stdint.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int property_get(const char *key, char *value, const char *default_value);
+#ifdef __cplusplus
+}
+#endif
+
+inline const char* BtmGetDefaultName()
+{
+	char device[92];
+	property_get("ro.product.device", device, "");
+
+        if (strstr(device, "douglas") != NULL) {
+            return "Fire HD8 2017";
+        } else if (strstr(device, "giza") != NULL) {
+            return "Fire HD8 2016";
+        }
+
+	return "Fire HD8";
+}
+
+#define BTM_DEF_LOCAL_NAME   BtmGetDefaultName()
 
 #define BTA_AV_MAX_A2DP_MTU  1788
 #define BLE_INCLUDED TRUE
